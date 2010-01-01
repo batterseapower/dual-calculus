6c10df7b3303be07e7d33a939234c46e9ce08b0e
Max Bolingbroke <batterseapower@hotmail.com>
2010-01-01 17:49:13 +0000

    More and better-commented examples

--- Main.hs
+++ Main.hs
@@ -15,35 +15,64 @@ normalise step s = s : case step s of
   Just s  -> normalise step s
 
 
-example1 = LetRec "ones" (App (App (LamVar "Cons") (LamVar "one")) (LamVar "ones"))
-                  (LamVar "ones")
+-- letrec ones = one : ones in ones
+--
+-- Simple demonstration of infinite data
+lamExample1 = LetRec "ones" (App (App (LamVar "Cons") (LamVar "one")) (LamVar "ones"))
+                     (LamVar "ones")
 
-example2 = LetRec "map" (LamLam "f" $ LamLam "xs" $ Case (LamVar "xs") [(Just "Nil", [], LamVar "Nil_wrap"), (Just "Cons", ["y", "ys"], App (App (LamVar "Cons_wrap") (App (LamVar "f") (LamVar "x"))) $ App (App (LamVar "map") (LamVar "f")) (LamVar "ys"))])
-                  (App (App (LamVar "map") (LamVar "inc")) $ App (App (LamVar "map") (LamVar "double")) (LamVar "input"))
+-- let map = \f. \xs. case xs of Nil -> Nil_wrap; Cons y ys -> Cons_wrap (f y) (map f ys)
+-- in map inc (map double input)
+--
+-- Advanced example for supercompilation experiments
+lamExample2 = LetRec "map" (LamLam "f" $ LamLam "xs" $ Case (LamVar "xs") [(Just "Nil", [], LamVar "Nil_wrap"), (Just "Cons", ["y", "ys"], App (App (LamVar "Cons_wrap") (App (LamVar "f") (LamVar "y"))) $ App (App (LamVar "map") (LamVar "f")) (LamVar "ys"))])
+                     (App (App (LamVar "map") (LamVar "inc")) $ App (App (LamVar "map") (LamVar "double")) (LamVar "input"))
 
-example3_term
+
+-- let f = \x. x
+-- in (f (), f 2)
+--
+-- Useful for getting a feel of how lambdas work in their primitive form
+dualExample1Term
   = letin "f" (CallByName.lam "x" (Var "x"))
               (Bind (CallByName.app (Var "f") (Tup []) `Cut`
                     CoBind "r1" (CallByName.app (Var "f") (Var "2") `Cut`
                                 CoBind "r2" (Tup [Var "r1", Var "r2"] `Cut`
                                              CoVar"c"))) "c") 
 
-example3 = example3_term `Cut` CoVar "halt"
+dualExample1 = dualExample1Term `Cut` CoVar "halt"
 
+-- let id = (\x. x) (\x. x)
+-- in id id
+--
+-- Useful for showing the difference between call by name and call by need,
+-- since call by need evaluates the application in "id" only once
+dualExample2 = letin "id" (app (Lam "x" (Var "x")) (Lam "x" (Var "x")))
+                          (app (Var "id") (Var "id")) `Cut` CoVar "halt"
 
-main = do
+dualExample1Main = do
      -- Just show what we're going to work on
     header "Original"
-    print $ pPrint example3
-     -- Obtain the tuple from example3
+    print $ pPrint dualExample1
+     -- Obtain the tuple from example1
     header "Call by name"
-    mapM_ (print . pPrint) $ normalise CallByName.step example3
+    mapM_ (print . pPrint) $ normalise CallByName.step dualExample1
      -- Place demand on the first component of that tuple
     header "Call by name, first component"
-    mapM_ (print . pPrint) $ normalise CallByName.step $ example3_term `Cut` CoTup 0 (CoVar "halt")
+    mapM_ (print . pPrint) $ normalise CallByName.step $ dualExample1Term `Cut` CoTup 0 (CoVar "halt")
      -- Right, what does that look like in need?
     header "Call by need, first component"
-    mapM_ (print . pPrint) $ normalise CallByNeed.step $ example3_term `Cut` CoTup 0 (CoVar "halt")
+    mapM_ (print . pPrint) $ normalise CallByNeed.step $ dualExample1Term `Cut` CoTup 0 (CoVar "halt")
+
+dualExample2Main = do
+    header "Original"
+    print $ pPrint dualExample2
+    header "Call by name"
+    mapM_ (print . pPrint) $ normalise CallByName.step dualExample2
+    header "Call by need"
+    mapM_ (print . pPrint) $ normalise CallByNeed.step dualExample2
+
+main = dualExample2Main
 
 header s = putStrLn $ unwords [replicate 10 '=', s, replicate 10 '=']
 
